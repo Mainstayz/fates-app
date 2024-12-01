538c4986da9dc7b84c3cfba846da38877469dc1e
Mainstayz <hezongzhu@gmail.com>
2024-12-01 16:32:36 +0800

    feat(message-box): enhance message box functionality with event handling
    - Implemented mouse enter and leave event listeners for the message box window, allowing it to show and hide based on tray icon interactions.
    - Updated window creation logic to include new capabilities for showing, hiding, and focusing the message box.
    - Refactored setup logic to ensure proper cleanup of event listeners on component unmount.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,69 @@
 <script lang="ts">
     import App from "./app.svelte";
-    import { createWindow } from "../windows";
+    import { listen } from "@tauri-apps/api/event";
+    import { createWindow, getWin } from "../windows";
+    import { onMount } from "svelte";
+    import { LogicalPosition } from '@tauri-apps/api/window'
 
-    createWindow("message-box", {
-        title: "Message Box",
-        url: "/message-box",
-        width: 400,
-        height: 320,
-        decorations: true,
-        resizable: true,
-        alwaysOnTop: true,
-        center: true,
-    }).then((window) => {
-        window.size().then((size) => {
-            console.log(`window size: ${size}`);
+    export let messageBoxWindowWidth = 280
+    export let messageBoxWindowHeight = 100
+
+    let unlisteners: Array<() => void> = [];
+    async function setupMessageBoxWin() {
+        const win = await createWindow("message-box", {
+            title: "Message Box",
+            url: "/message-box",
+            width: messageBoxWindowWidth,
+            height: messageBoxWindowHeight,
+            decorations: true,
+            resizable: false,
+            alwaysOnTop: true,
+            center: true,
+            visible: true,
         });
-    }).catch((error) => {
-        console.error(`window creation failed: ${error}`);
+
+        const unlisten = await listen("tray_mouseenter", async (event) => {
+            console.log("tray_mouseenter", event);
+            const win = await getWin("message-box");
+            if (win) {
+                let position = event.payload as { x: number, y: number };
+                await win.setAlwaysOnTop(true);
+                await win.setFocus();
+                // await win.setPosition(new LogicalPosition(position.x - messageBoxWindowWidth / 2, window.screen.availHeight - messageBoxWindowHeight))
+                await win.show();
+            }
+        });
+
+        const unlisten2 = await listen("tray_mouseleave", async (event) => {
+            console.log("tray_mouseleave", event);
+            const win = await getWin("message-box");
+            if (win) {
+                await win.hide();
+            }
+        });
+
+        // tauri://blur
+        const unlisten3 = await listen("tauri://blur", async (event) => {
+            console.log("tauri://blur", event);
+            const win = await getWin("message-box");
+            if (win) {
+                await win.hide();
+            }
+        });
+
+        unlisteners.push(unlisten);
+        unlisteners.push(unlisten2);
+        unlisteners.push(unlisten3);
+
+    }
+    onMount( () => {
+        setupMessageBoxWin();
+        return () => {
+            unlisteners.forEach(unlisten => unlisten?.());
+        };
     });
 
+    setupMessageBoxWin();
 </script>
 
 <main class="noSelect w-full h-full">
