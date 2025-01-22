08f2c25a0525f8ed68faf2a1764957d87e14a542
pillar ho <hezongzhu@gmail.com>
2025-01-23 01:05:56 +0800

    feat(calendar): Add support for calendar functionality on Windows platforms - Add windows dependency in Cargo.toml - Implement calendar settings opening, permission requesting and event fetching for Windows platforms. - Optimize calendar event handling logic for macOS platforms.

--- src-tauri/src/calendar.rs
+++ src-tauri/src/calendar.rs
@@ -1,5 +1,5 @@
 #[cfg(target_os = "macos")]
-use block2::{ Block, RcBlock };
+use block2::{Block, RcBlock};
 #[cfg(target_os = "macos")]
 use dispatch::Queue;
 #[cfg(target_os = "macos")]
@@ -7,20 +7,25 @@ use objc2::runtime::Bool;
 #[cfg(target_os = "macos")]
 use objc2_app_kit::NSWorkspace;
 #[cfg(target_os = "macos")]
-use objc2_event_kit::{ EKCalendarType, EKEntityType, EKEventStore };
+use objc2_event_kit::{EKCalendarType, EKEntityType, EKEventStore};
 #[cfg(target_os = "macos")]
 use objc2_foundation::{
-    is_main_thread,
-    ns_string,
-    NSCalendar,
-    NSCalendarUnit,
-    NSDate,
-    NSError,
-    NSISO8601DateFormatOptions,
-    NSISO8601DateFormatter,
-    NSURL,
+    is_main_thread, ns_string, NSCalendar, NSCalendarUnit, NSDate, NSError,
+    NSISO8601DateFormatOptions, NSISO8601DateFormatter, NSURL,
 };
-use serde::{ Deserialize, Serialize };
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+#[cfg(target_os = "windows")]
+use windows::{
+    core::HSTRING,
+    ApplicationModel::Appointments::{
+        Appointment, AppointmentManager, AppointmentStore, AppointmentStoreAccessType,
+    },
+    Foundation::{DateTime, TimeSpan, Uri},
+    System::Launcher,
+};
+
+use serde::{Deserialize, Serialize};
 use tauri::command;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -40,18 +45,21 @@ impl From<&objc2_event_kit::EKEvent> for CalendarMatter {
     fn from(event: &objc2_event_kit::EKEvent) -> Self {
         unsafe {
             let formatter = NSISO8601DateFormatter::new();
-            let options =
-                NSISO8601DateFormatOptions::NSISO8601DateFormatWithInternetDateTime |
-                NSISO8601DateFormatOptions::NSISO8601DateFormatWithDashSeparatorInDate |
-                NSISO8601DateFormatOptions::NSISO8601DateFormatWithColonSeparatorInTime |
-                NSISO8601DateFormatOptions::NSISO8601DateFormatWithColonSeparatorInTimeZone |
-                NSISO8601DateFormatOptions::NSISO8601DateFormatWithFractionalSeconds;
+            let options = NSISO8601DateFormatOptions::NSISO8601DateFormatWithInternetDateTime
+                | NSISO8601DateFormatOptions::NSISO8601DateFormatWithDashSeparatorInDate
+                | NSISO8601DateFormatOptions::NSISO8601DateFormatWithColonSeparatorInTime
+                | NSISO8601DateFormatOptions::NSISO8601DateFormatWithColonSeparatorInTimeZone
+                | NSISO8601DateFormatOptions::NSISO8601DateFormatWithFractionalSeconds;
             formatter.setFormatOptions(options);
 
             let id = event.eventIdentifier().unwrap().to_string();
             let title = event.title().to_string();
-            let start_date = formatter.stringFromDate(event.startDate().as_ref()).to_string();
-            let end_date = formatter.stringFromDate(event.endDate().as_ref()).to_string();
+            let start_date = formatter
+                .stringFromDate(event.startDate().as_ref())
+                .to_string();
+            let end_date = formatter
+                .stringFromDate(event.endDate().as_ref())
+                .to_string();
 
             let mut description: String = "".to_string();
             if let Some(notes) = event.notes() {
@@ -85,18 +93,29 @@ impl From<&objc2_event_kit::EKEvent> for CalendarMatter {
 pub async fn open_calendar_setting() -> Result<(), String> {
     #[cfg(target_os = "macos")]
     unsafe {
-        let nsstr = ns_string!(
-            "x-apple.systempreferences:com.apple.preference.security?Privacy_Calendars"
-        );
+        let nsstr =
+            ns_string!("x-apple.systempreferences:com.apple.preference.security?Privacy_Calendars");
         let url = NSURL::URLWithString(nsstr).unwrap();
         let workspace = NSWorkspace::sharedWorkspace();
         workspace.openURL(&url);
     }
-    #[cfg(not(target_os = "macos"))]
+    #[cfg(target_os = "windows")]
     {
-        log::warn!("Calendar settings are only supported on macOS");
+        use windows::{Foundation::Uri, System::Launcher};
+
+        async fn open_calendar_settings_windows() -> Result<(), String> {
+            // let uri = Uri::CreateUri(&HSTRING::from("ms-settings:privacy-calendar"))?;
+            // Launcher::LaunchUriAsync(&uri)?.await?;
+            Ok(())
+        }
+
+        open_calendar_settings_windows().await
     }
-    Ok(())
+    #[cfg(not(any(target_os = "macos", target_os = "windows")))]
+    {
+        log::warn!("Calendar settings are only supported on macOS and Windows");
+        Ok(())
+    }
 }
 
 #[command]
@@ -108,9 +127,66 @@ pub async fn get_calendar_permission_status() -> i32 {
         // 0: NotDetermined, 1: Restricted, 2: Denied, 3: FullAccess, 4: WriteOnly
         status.0 as i32
     }
-    #[cfg(not(target_os = "macos"))]
+    #[cfg(target_os = "windows")]
     {
-        log::warn!("Calendar permissions are only supported on macOS");
+        log::info!("Requesting calendar access...");
+        let store =
+            AppointmentManager::RequestStoreAsync(AppointmentStoreAccessType::AllCalendarsReadOnly)
+                .unwrap()
+                .await
+                .unwrap();
+        log::info!("Calendar access result: {:?}", store);
+        let calenders = store
+            .FindAppointmentCalendarsAsync()
+            .unwrap()
+            .await
+            .unwrap();
+        let size = calenders.Size().unwrap();
+        if size == 0 {
+            log::info!("No calendars found");
+        } else {
+            log::info!("Found {} calendars", size);
+            for calendar in calenders {
+                // https://learn.microsoft.com/zh-cn/uwp/api/windows.applicationmodel.appointments.appointmentcalendar?view=winrt-26100
+                log::info!("Calendar: {:?}", calendar.DisplayName());
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs() as i64;
+                let start_time = DateTime {
+                    UniversalTime: (now * 10_000_000) + 116444736000000000,
+                };
+
+                // 24小时后
+                let duration = TimeSpan {
+                    Duration: 365 * 24 * 60 * 60 * 10_000_000i64,
+                };
+
+                let appointments = match calendar.FindAppointmentsAsync(start_time, duration) {
+                    Ok(future) => {
+                        match future.await {
+                            Ok(appointments) => appointments,
+                            Err(e) => {
+                                log::error!("Error awaiting appointments: {}", e);
+                                return 0;
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        log::error!("Error getting appointments: {}", e);
+                        return 0;
+                    }
+                };
+                if appointments.Size().unwrap() == 0 {
+                    log::info!("No appointments found");
+                    return 0;
+                }
+                log::info!("Found {} appointments", appointments.Size().unwrap());
+                for appointment in appointments {
+                    log::info!("Appointment: {:?}", appointment.Subject());
+                }
+            }
+        }
         0 // NotDetermined
     }
 }
@@ -126,9 +202,8 @@ pub async fn request_calendar_access() -> Result<(), String> {
             let completion_handler = RcBlock::new(move |granted: Bool, error: *mut NSError| {
                 log::info!("Calendar access result: {}", granted.as_bool());
             });
-            let completion_handler_ptr = &*completion_handler as *const Block<_> as *mut Block<
-                dyn Fn(Bool, *mut NSError)
-            >;
+            let completion_handler_ptr =
+                &*completion_handler as *const Block<_> as *mut Block<dyn Fn(Bool, *mut NSError)>;
             store.requestFullAccessToEventsWithCompletion(completion_handler_ptr);
         });
     }
@@ -159,19 +234,15 @@ pub async fn get_calendar_events() -> Result<Vec<CalendarMatter>, String> {
         let end_date = calendar.dateFromComponents(&start_components).unwrap();
         log::info!("End date: {:?}", end_date);
 
-        let predicate = store.predicateForEventsWithStartDate_endDate_calendars(
-            &start_date,
-            &end_date,
-            None
-        );
+        let predicate =
+            store.predicateForEventsWithStartDate_endDate_calendars(&start_date, &end_date, None);
         let events = store.eventsMatchingPredicate(&predicate);
         log::info!("Events count: {:?}", events.count());
         let mut result = Vec::new();
         for event in events.iter() {
             let calendar = event.calendar().unwrap();
-            if
-                calendar.r#type() == EKCalendarType::Subscription ||
-                calendar.r#type() == EKCalendarType::Birthday
+            if calendar.r#type() == EKCalendarType::Subscription
+                || calendar.r#type() == EKCalendarType::Birthday
             {
                 // 订阅的日历和生日日历不显示
                 log::info!(
